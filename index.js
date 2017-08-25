872f7793cbe1cd589bd42dce83c3adef28ddfafb
Phil Gebauer <gebauer.phil@gmail.com>
2017-08-25 12:22:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ function get_browser() {
 
    function findVersion(versionNum){
      if(full != true && versionNum == "9") {
-         return 
          alert("You’re using Internet Explorer " + versionNum + ", which is an outdated web browser. That means things on this site won’t look right. And after January 1, 2018, things won’t display at all. The best way to fix the problem is to update your browser with the current version.");
          sessionStorage.setItem('tester', true);
          full = sessionStorage.getItem('tester')
