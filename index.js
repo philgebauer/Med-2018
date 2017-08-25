39475a9fdf885447c04d6e10b7851322bae24a4a
Phil Gebauer <gebauer.phil@gmail.com>
2017-08-25 12:24:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,3 @@
-
-
-// function get_browser() {
-//     var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-//     if(/trident/i.test(M[1])){
-//         tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
-//         return {name:'IE',version:(tem[1]||'')};
-//         }
-//     if(M[1]==='Chrome'){
-//         tem=ua.match(/\bOPR|Edge\/(\d+)/)
-//         if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-//         }
-//     M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-//     if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
-//     return {
-//       name: M[0],
-//       version: M[1]
-//     };
-//  }
-//
-//  var browser = get_browser();
-//
-//  console.log(browser.version);
-//
-//  function findVersion(versionNum){
-//    if(typeof(Storage) !== "undefined") {
-//      if (versionNum == "9"){
-//        alert("You’re using Internet Explorer " + versionNum + ", which is an outdated web browser. That means things on this site won’t look right. And after January 1, 2018, things won’t display at all. The best way to fix the problem is to update your browser with the current version.");
-//      } else if (versionNum == "10") {
-//        alert("We’ve noticed you’re using Internet Explorer " + versionNum + ", an outdated web browser. That means things on our site won’t look right. And after January 1, 2018, things won’t display at all.\n\nWhat’s the fix? Get the current version of Internet Explorer or Chrome.");
-//      } else {
-//        alert("You are using IE" + versionNum);
-//        }
-//      }
-//    }
-//
-//  findVersion(browser.version);
 var full;
 
 function get_browser() {
@@ -66,10 +29,10 @@ function get_browser() {
 
    function findVersion(versionNum){
      if(full != true && versionNum == "9") {
-         alert("You’re using Internet Explorer " + versionNum + ", which is an outdated web browser. That means things on this site won’t look right. And after January 1, 2018, things won’t display at all. The best way to fix the problem is to update your browser with the current version.");
          sessionStorage.setItem('tester', true);
          full = sessionStorage.getItem('tester')
          console.log("var full is " + full);
+         alert("You’re using Internet Explorer " + versionNum + ", which is an outdated web browser. That means things on this site won’t look right. And after January 1, 2018, things won’t display at all. The best way to fix the problem is to update your browser with the current version.");
        } else if (typeof(Storage) !== "undefined" && versionNum == "10") {
          alert("We’ve noticed you’re using Internet Explorer " + versionNum + ", an outdated web browser. That means things on our site won’t look right. And after January 1, 2018, things won’t display at all.\n\nWhat’s the fix? Get the current version of Internet Explorer or Chrome.");
        } else {
