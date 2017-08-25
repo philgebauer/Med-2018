c82b5a739c6cf73be79a9e01de99033433e550d6
Phil Gebauer <gebauer.phil@gmail.com>
2017-08-25 10:54:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function get_browser() {
    function findVersion(versionNum){
      if(full === null && versionNum == "9") {
          alert("You’re using Internet Explorer " + versionNum + ", which is an outdated web browser. That means things on this site won’t look right. And after January 1, 2018, things won’t display at all. The best way to fix the problem is to update your browser with the current version.");
-         localStorage.setItem('tester','true');
+         return full = localStorage.setItem('tester','true');
        } else if (typeof(Storage) !== "undefined" && versionNum == "10") {
          alert("We’ve noticed you’re using Internet Explorer " + versionNum + ", an outdated web browser. That means things on our site won’t look right. And after January 1, 2018, things won’t display at all.\n\nWhat’s the fix? Get the current version of Internet Explorer or Chrome.");
        } else {
