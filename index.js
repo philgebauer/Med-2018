97e4349e60eb23ceb60878197840dd36db881748
Phil Gebauer <gebauer.phil@gmail.com>
2017-08-25 10:56:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var full;
+
 function get_browser() {
     var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
     if(/trident/i.test(M[1])){
@@ -19,16 +21,16 @@ function get_browser() {
  var browser = get_browser();
 
  console.log(browser.version);
- var full = localStorage.getItem('tester')
+full = localStorage.getItem('tester')
  // localStorage.setItem('tester', null);
  
  console.log("var full is " + full);
 
 
    function findVersion(versionNum){
-     if(full === null && versionNum == "9") {
+     if(full == null && versionNum == "9") {
          alert("You’re using Internet Explorer " + versionNum + ", which is an outdated web browser. That means things on this site won’t look right. And after January 1, 2018, things won’t display at all. The best way to fix the problem is to update your browser with the current version.");
-         return full = localStorage.setItem('tester','true');
+         full = localStorage.setItem('tester','true');
        } else if (typeof(Storage) !== "undefined" && versionNum == "10") {
          alert("We’ve noticed you’re using Internet Explorer " + versionNum + ", an outdated web browser. That means things on our site won’t look right. And after January 1, 2018, things won’t display at all.\n\nWhat’s the fix? Get the current version of Internet Explorer or Chrome.");
        } else {
